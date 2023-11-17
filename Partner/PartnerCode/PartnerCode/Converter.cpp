01b798af7767a6c84f5da670af4a858c503c6e41
(Student  C00297034) Cialan Patterson <C00297034@setu.ie>
2023-11-17 10:46:50 +0000

    leaderOutput

--- Partner/PartnerCode/PartnerCode/Converter.cpp
+++ Partner/PartnerCode/PartnerCode/Converter.cpp
@@ -11,14 +11,13 @@ int main()
 
 char printMenu()
 {
-	char choice = ' '; //user input choice
-	std::cout << "Please enter an upper casre letter to convert from left to right and lower to convert from right to left" << std::endl;
+	char choice = ' ';
+	std::cout << "Please enter an upper case letter to convert from left to right and lower to convert from right to left" << std::endl;
 	std::cout << "[A] Degrees <---> Radians [a]" << std::endl;
-	std::cout << "[B] Centimetres <---> Feet & Inches [b]" << std::endl;
+	std::cout << "[B] Centimetres <---> Feet&Inches [b]" << std::endl;
 	std::cout << "[C] Litres <---> Gallons [c]" << std::endl;
 
 	std::cout << "[Q] Exit [---] Exit [q]" << std::endl;
-	std::cout << "Enter your Choice: ";
 	std::cin >> choice;
 	return choice;
 }
